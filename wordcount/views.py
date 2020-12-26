c9a3d017a063beb9b27d7ad1fb55c0c64856c631
Andrew Merkur <mbp@andrews-mbp.lan>
2020-12-26 12:46:19 -0800

    first

---
+++ wordcount/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    
+    worddict = {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+
+    lwords = len(wordlist)
+    
+    sortedwords = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':lwords, 'sortedwords':sortedwords}) 
+    #references {% url ='count %} in html file
+
+def about(request):
+    return render(request, 'about.html')
